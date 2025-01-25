5460eacc4f365b25cc83bdd7e956fa2d44a37989
Afif Sauqil Arifin <58023141+afifsauqil@users.noreply.github.com>
2025-01-25 10:47:34 +0700

    update content index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: [".index.html/**/*.{html,js}"],
   darkMode: "class",
   theme: {
     extend: {
